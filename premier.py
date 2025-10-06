b52b03120a03afb326cff8496f95c94f5919b0b8
Yann <yann.popov@hotmail.fr>
2025-10-06 09:42:05 +0200

    ok

---
+++ premier.py
@@ -0,0 +1,9 @@
+n=int(input("entrer un nombre:"))
+premier=True
+for diviseur in range (2,n):
+    reste=n% diviseur
+    if reste==0:
+        print(n,"n'est pas premier car divisible par",diviseur) 
+        premier=False
+        
+print(n,"est premier !")
